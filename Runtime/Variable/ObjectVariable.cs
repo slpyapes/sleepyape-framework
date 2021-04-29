2eca027f95af4507dfbc83c22de8e4ff3bb1b6cd
sickape <sickape99@gmail.com>
2021-04-30 02:56:25 +0700

    add object variable

---
+++ Runtime/Variable/ObjectVariable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SleepyApe
+{
+    [CreateAssetMenu(menuName = "SleepyApe/Variable/Object", order = -1000)]
+    public class ObjectVariable : Variable<Object>
+    {
+    }
+}
