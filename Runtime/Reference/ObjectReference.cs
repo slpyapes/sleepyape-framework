070c003549de64e2f3ff8bedc1c2e9363739ed49
sickape <sickape99@gmail.com>
2021-06-16 20:03:19 +0700

    feat: add ValueAs<> on ObjectReference

--- Runtime/Reference/ObjectReference.cs
+++ Runtime/Reference/ObjectReference.cs
@@ -6,5 +6,6 @@ namespace SleepyApe
     [Serializable]
     public class ObjectReference : Reference<Object, ObjectVariable>
     {
+        public T ValueAs<T>() where T : Object => (T)Value;
     }
 }
