9e70c4f998c1188c07f34c2bf3f20b567f647473
sickape <sickape99@gmail.com>
2021-06-17 11:52:10 +0700

    feat: expose UseVariable on object derived from Reference

--- Runtime/Reference/Reference.cs
+++ Runtime/Reference/Reference.cs
@@ -8,8 +8,19 @@ namespace SleepyApe
         [SerializeField] private TType _constantValue = default;
         [SerializeField] private TVariable _variableValue = default;
 
+        /// <summary>
+        /// Return reference value
+        /// </summary>
         public TType Value => _useConstant ? _constantValue : _variableValue.Value;
 
+        /// <summary>
+        /// Return variable if UseVariable is true
+        /// </summary>
         public TVariable Variable => !_useConstant ? _variableValue : null;
+
+        /// <summary>
+        /// Is this reference use variable?
+        /// </summary>
+        public bool UseVariable => !_useConstant;
     }
 }
