6b15234ffe2751b12a83aa2e1c7f03bc5ca4a142
sickape <sickape99@gmail.com>
2021-09-06 17:08:29 +0700

    feat: add `Transform.ResetRotation()` extensions

--- Runtime/Extension/TransformExtension.cs
+++ Runtime/Extension/TransformExtension.cs
@@ -7,13 +7,17 @@ namespace SleepyApe
         public static void Reset(this Transform target)
         {
             ResetPosition(target);
-            target.rotation = Quaternion.Euler(0f, 0f, 0f);
-            target.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            ResetRotation(target);
         }
 
         public static void ResetPosition(this Transform target)
         {
             target.position = Vector3.zero;
+        }
+
+        public static void ResetRotation(this Transform target)
+        {
+            target.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
     }
 }
